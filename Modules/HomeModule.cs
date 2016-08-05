b13f98ab6b444e13a69adc25f37bbddb3ab27247
yichiang <yi_chiang@outlook.com>
2016-08-05 09:51:30 -0700

    add delete route

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,32 @@ namespace BandTracker
         Dictionary<string,object> model=Member.AllData("Welcome");
         return View["index.cshtml", model];
       };
+      Post["/venue/new"]=_=>{
+        Venue newVenue= new Venue(Request.Form["new-venue-name"]);
+        newVenue.Save();
+        Dictionary<string,object> model=Member.AllData("Welcome");
+        return View["index.cshtml", model];
+      };
+      Delete["/delete/venue/{id}"]=parameters=>{
+        Venue newVenue= Venue.Find(parameters.id);
+        newVenue.Delete();
+        Dictionary<string,object> model=Member.AllData("Welcome");
+        return View["index.cshtml", model];
+      };
+
+
+      Post["/band/new"]=_=>{
+        Band newBand= new Band(Request.Form["new-band-name"],Request.Form["new-band-date"]);
+        newBand.Save();
+        Dictionary<string,object> model=Member.AllData("Welcome");
+        return View["index.cshtml", model];
+      };
+      Delete["/delete/band/{id}"]=parameters=>{
+        Band newBand= Band.Find(parameters.id);
+        newBand.Delete();
+        Dictionary<string,object> model=Member.AllData("Welcome");
+        return View["index.cshtml", model];
+      };
     }
   }
 }
